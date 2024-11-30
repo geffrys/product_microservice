7ff8a9c9148f2dd8b580f1a5c65d0db2d9bf37c8
geffrys <geffry.ospina@gmail.com>
2024-11-30 03:39:38 -0500

    Ft: product microservice.

---
+++ product-service/src/main/java/com/geffry/product/ProductserviceApplication.java
@@ -0,0 +1,13 @@
+package com.geffry.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductserviceApplication.class, args);
+	}
+
+}
