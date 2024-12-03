7e70c57ef03a687f02783c6b905d560b4c3288f1
geffrys <geffry.ospina@gmail.com>
2024-12-03 00:41:53 -0500

    Ft: container test structure and dependencies added

--- product-service/src/test/java/com/geffry/product/ProductserviceApplicationTests.java
+++ product-service/src/test/java/com/geffry/product/ProductserviceApplicationTests.java
@@ -1,13 +1,38 @@
 package com.geffry.product;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistrar;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@AutoConfigureMockMvc // to allow autowired IoC behavior to mockMvc object
+@Testcontainers // add test containers
 class ProductserviceApplicationTests {
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Container
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.4");
+
+	@DynamicPropertySource // to add properties dynamically introducing it, to springboot application context.
+	// adding custom properties for data source.
+	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry){
+		dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+
+	}
 
 	@Test
-	void contextLoads() {
+	void shouldCreateProduct() {
+		mockMvc.perform(MockMvcTester.)
 	}
 
 }
