5791e770258b5391507e994ce4ce938da695ebdd
geffrys <geffry.ospina@gmail.com>
2024-12-06 16:58:35 -0500

    Ft: ShouldCreateProduct test has been finished.

--- product-service/src/test/java/com/geffry/product/ProductserviceApplicationTests.java
+++ product-service/src/test/java/com/geffry/product/ProductserviceApplicationTests.java
@@ -1,17 +1,29 @@
 package com.geffry.product;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.geffry.product.dto.ProductRequest;
+import com.geffry.product.dto.ProductResponse;
+import com.geffry.product.repository.ProductRepository;
+import com.mongodb.assertions.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistrar;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc // to allow autowired IoC behavior to mockMvc object
@@ -19,6 +31,10 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 class ProductserviceApplicationTests {
 	@Autowired
 	private MockMvc mockMvc;
+	@Autowired
+	private ObjectMapper objectMapper; // to convert an object to json object
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Container
 	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.4");
@@ -31,8 +47,24 @@ class ProductserviceApplicationTests {
 	}
 
 	@Test
-	void shouldCreateProduct() {
-		mockMvc.perform(MockMvcTester.)
+	void shouldCreateProduct() throws Exception {
+		ProductRequest productRequest = getProductRequest();
+		String productRequestString = objectMapper.writeValueAsString(productRequest);
+		
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(productRequestString))
+				.andExpect(status().isCreated())
+		;
+		Assertions.assertTrue(productRepository.findAll().size() == 1);
+	}
+
+	private ProductRequest getProductRequest() {
+		return ProductRequest.builder()
+				.name("iPhone 15")
+				.description("iPhone 15 pro max")
+				.price(BigDecimal.valueOf(1200))
+				.build();
 	}
 
 }
